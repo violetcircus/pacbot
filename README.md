83b0543e0b5b9627fcddc4d603848ad912f2cc24
violet <iaresmarts@gmail.com>
2025-07-19 02:12:41 +0100

    getting the ability to send a message

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # pacbot!
 pacbot is a simple discord bot that provides you with information about packages in the various arch package repositories on command. It's written in Go because that's the language I'm learning right now (also I really like its standard library).
 
+none of it works yet! im mostly just tinkering right now. I'm leaving it public so my friends can see the source code when i stick it in their server.
+
 ## Usage:
 - `!pacbot "QUERY"`: search the core, extra and multilib repositories for packages that include the string you input.
 - `!pac "QUERY"`: search only official repositories
--- main.go
+++ main.go
@@ -1,7 +1,51 @@
 package main
 
-import ()
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+)
+
+type messageParams struct {
+	apiVersion int64
+	parameters map[string]string
+	token      string
+	channelID  string
+	message    string
+}
+
+const contentType string = "application/json"
+const userAgent string = "pacbot https://github.com/violetcircus/pacbot"
 
 func main() {
+	message := messageParams{
+		apiVersion: 10,
+	}
+	sendMessage(message)
+}
 
+func sendMessage(msg messageParams) {
+	targetUrl := fmt.Sprintf("https://discordapp.com/api/channels/%s/messages", strconv.FormatInt(msg.apiVersion, 10))
+
+	// assemble params into a suitable format for post request
+	postBody := url.Values{} // creates empty map (std. lib thing for handling URL-encoded form data)
+	for a, b := range msg.parameters {
+		path, err := url.PathUnescape(b) // unescapes value in parameters key:value pair for use in form
+		if err != nil {
+			log.Fatal(err)
+		}
+		postBody.Set(a, path) // adds the key, value pair to the post body with the value now unescaped for use in a form
+	}
+
+	req, error := http.NewRequest("POST", targetUrl, strings.NewReader(postBody.Encode()))
+	if error != nil {
+		log.Fatal("request failed lol")
+	}
+	req.Header.Add("Authorization", msg.token)
+	req.Header.Add("User-Agent", userAgent)
+	req.Header.Add("Content-Type", contentType)
 }
